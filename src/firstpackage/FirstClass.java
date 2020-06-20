02f838be15233aef4afa6a1b90dca85a4856f68a
tknoozkurt <67170670+tknoozkurt@users.noreply.github.com>
2020-06-20 15:31:06 -0400

    Update FirstClass.java

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,11 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("First commit from from local repository");              
-		
-		
-		System.out.println("Second commit from local repository");
-		
+		System.out.println("First commit from from local repository");                 
+                System.out.println("Second commit from local repository");
 		System.out.println("Conflict yapmak istiyoruz");
 		System.out.println("GLIBA conflict yaptikl");
 	}
