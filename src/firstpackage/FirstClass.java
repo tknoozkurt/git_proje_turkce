2f97115d632de70e86ab315e5f7848cd1551395d
Tekin <tekinozkurt@tekins-MacBook-Air.local>
2020-06-20 15:41:55 -0400

    2.son  conflict

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,8 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 
 		System.out.println("First commit from from local repository");                 
-                System.out.println("Second commit from local repository");
-		System.out.println("Conflict yapmak istiyoruz");
+        System.out.println("Second commit from local repository");
+		System.out.println("2. kez Conflict yapmak istiyoruz");
 		System.out.println("GLIBA conflict yaptikl");
 
 	}
