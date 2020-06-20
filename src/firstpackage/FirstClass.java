14cbe71ca16b55ef3127ac1db3f3fea93fb10588
tknoozkurt <67170670+tknoozkurt@users.noreply.github.com>
2020-06-20 15:44:31 -0400

    Update FirstClass.java
    artik cozum ariyoruz

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,7 +6,7 @@ public class FirstClass {
 
 		System.out.println("First commit from from local repository");                 
                 System.out.println("Second commit from local repository");
-		System.out.println("Conflict yapmak istiyoruz");
+		System.out.println("We want to make the second conflict turcesi  yapmak istiyoruz");
 		System.out.println("GLIBA conflict yaptikl");
 
 	}
