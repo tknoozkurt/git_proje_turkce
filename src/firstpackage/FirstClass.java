13d23ceee2c68e94d7f798fce5d5a03f57f89760
Tekin <tekinozkurt@tekins-MacBook-Air.local>
2020-06-20 14:15:09 -0400

    first commit from local repo

---
+++ src/firstpackage/FirstClass.java
@@ -0,0 +1,14 @@
+package firstpackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("First commit from from local repository");              
+		
+		
+		
+		
+	}
+
+}
