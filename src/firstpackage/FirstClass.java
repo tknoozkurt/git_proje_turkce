7291ca632780d3a811f98a6874f734b1edbe759f
tknoozkurt <67170670+tknoozkurt@users.noreply.github.com>
2020-06-20 15:38:15 -0400

    Update FirstClass.java

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,17 +4,11 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
-		System.out.println("First commit from from local repository");              
-		System.out.println("Second commit from local repository");
-		System.out.println("Ucuncu commit from local repository");
-		System.out.println("First commit from from remote repository to local");
-=======
 		System.out.println("First commit from from local repository");                 
                 System.out.println("Second commit from local repository");
 		System.out.println("Conflict yapmak istiyoruz");
 		System.out.println("GLIBA conflict yaptikl");
->>>>>>> refs/remotes/origin/master
+
 	}
 	
 
