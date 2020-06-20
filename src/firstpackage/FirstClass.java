c006a4ed0909fdc7e951c088344014d6a1b21de6
Tekin <tekinozkurt@tekins-MacBook-Air.local>
2020-06-20 15:27:44 -0400

    ikinci commit from local repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,11 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 
 		System.out.println("First commit from from local repository");              
-		
-		
 		System.out.println("Second commit from local repository");
-		
-		System.out.println("Thrid commit from local repository");
+		System.out.println("Ucuncu commit from local repository");
 		System.out.println("First commit from from remote repository to local");
 	}
 	
